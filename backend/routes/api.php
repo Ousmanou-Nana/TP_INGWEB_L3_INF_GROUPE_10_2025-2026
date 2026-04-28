208047fab25dff1e64c5f00a0c2d1ba1ea216afb
Mamoud-code <nouroudinemahmoud8@gmail.com>
2026-04-28 02:55:34 +0100

    feat: systeme d'authentification

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::prefix("/v1")->group(function () {
-    require_once  __DIR__ . '/api.v1.php';
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
